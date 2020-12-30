b1356352e25a618a88c82c4bb2fec03163646eec
Renszo Camacho <varsovia84@gmail.com>
2020-12-30 17:41:50 +0100

    Accordion-Card. Functionality Js. Done.

--- src/vendor/main.js
+++ src/vendor/main.js
@@ -1,7 +1,10 @@
-const wrapQuestion = document.querySelectorAll(".main__wrapQuestion");
+let contentFaq = document.querySelector(".main__contentFaq");
+let wrapQuestion = document.querySelectorAll(".main__wrapQuestion");
 
 wrapQuestion.forEach((el) => {
   el.addEventListener("click", () => {
+    contentFaq.querySelector(".active").classList.remove("active");
+
     el.classList.toggle("active");
   });
 });
